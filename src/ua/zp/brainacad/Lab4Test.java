f3b02df81653645c4726e6c959b89a7246dde859
Horypavlovskyi <Horypavlovskyi@gmail.com>
2020-09-06 20:54:43 +0300

    v2

--- src/ua/zp/brainacad/Lab4Test.java
+++ src/ua/zp/brainacad/Lab4Test.java
@@ -70,26 +70,27 @@ public class Lab4Test {
         //3.1
         // TODO create two-dimensional array with size [3][4]
         int[][] array1 = new int[3][4];
-        array1[0][0] = 1;
-        array1[0][1] = 1;
-        array1[0][2] = 1;
-        array1[0][3] = 1;
-        array1[1][0] = 2;
-        array1[1][1] = 2;
-        array1[1][2] = 2;
-        array1[1][3] = 2;
-        array1[2][0] = 3;
-        array1[2][1] = 3;
-        array1[2][2] = 3;
-        array1[2][3] = 3;
+//        array1[0][0] = 1;
+//        array1[0][1] = 1;
+//        array1[0][2] = 1;
+//        array1[0][3] = 1;
+//        array1[1][0] = 2;
+//        array1[1][1] = 2;
+//        array1[1][2] = 2;
+//        array1[1][3] = 2;
+//        array1[2][0] = 3;
+//        array1[2][1] = 3;
+//        array1[2][2] = 3;
+//        array1[2][3] = 3;
         //3.2
         // TODO fill array with any numbers in cycles.
-//        for (int i = 0; i < array1.length; i++) {
-//            for (int j = 0; j < array1[i].length; j++) {
-//                System.out.print(array1[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array1[i].length; j++) {
+                array1[i][j] = i;
+                System.out.print(array1[i][j] + " ");
+            }
+            System.out.println();
+        }
         //3.3
         // TODO calc sum of all array elements and print result.
 //        int sum1 = 0;
